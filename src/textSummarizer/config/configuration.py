82ec6ded1041c6798abbc128e926e394b2b2c729
Jims Chacko <jimschacko@gmail.com>
2023-07-19 13:39:20 +0530

    Data Transformation done - Stage 03 completed.

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -2,6 +2,7 @@ from textSummarizer.constant import *
 from textSummarizer.utils.common import read_yaml,create_directories
 from textSummarizer.entity import (DataIngestionConfig)
 from textSummarizer.entity import (DataValidationConfig)
+from textSummarizer.entity import (DataTransformationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -44,5 +45,16 @@ class ConfigurationManager:
 
         return data_validation_config
     
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
 
+        create_directories([config.root_dir])
 
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name = config.tokenizer_name
+        )
+
+        return data_transformation_config
